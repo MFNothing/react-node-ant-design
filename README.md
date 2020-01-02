ef0fd42c80cc462db4df4c5bbd8fc94cdc07d9df
lym <liuyimin@sobey.com>
2020-01-02 20:17:31 +0800

    完成前端搭建,以及中台数据库连接

--- README.md
+++ README.md
@@ -13,3 +13,94 @@
     * `yarn add babel-plugin-import`
     * 在项目根目录建立 .babelrc 文件
 * 创建 _app.js 文件，所以页面引入的地方
+* 安装 `react-markdown` 用于解析markdown
+    * `yarn add react-markdown`
+* 安装 `markdown-navbar` 用于提供markdown导航功能
+    * `yarn add markdown-navbar`
+    * className： 可以为导航定义一个class名称，从而进行style样式的定义。
+    * source：要解析的内容，也就是你的Markdown内容。
+    * headingTopOffset:描点距离页面顶部的位置，默认值是0.
+    * ordered: 显示数字编码，默认是显示的，也就是true，设置为false就不显示了。
+    * 安装完要注意要引入css
+        * `import 'markdown-navbar/dist/navbar.css';`
+    * 有点问题，自己clone下来改一下，没有初始化替换的逻辑
+
+
+## service 中台
+
+* 全局安装egg.js的脚手架工具egg-init
+    * `sudo npm i egg-init -g`
+* 在 service 中使用 egg-init 脚手架
+    * `sudo egg-init --type=simple`
+* `npm run dev` 时，文件不可以访问的话，要使用 sudo
+* 我们的所有数据的获得和业务逻辑的操作都是通过中台实现的，也就是说中台只提供接口，这里的设计我们采用RESTful的规则，让egg为前端提供Api接口，实现中台主要的功能。
+    * 特点 前后端分离，简单且有约束性
+    * GET(SELECT) ： 从服务端取出资源，可以同时取出一项或者多项。
+    * POST(CREATE) ：在服务器新建一个资源。
+    * PUT(UPDATE) ：在服务器更新资源（客户端提供改变后的完整资源）。
+    * DELETE(DELETE) ：从服务器删除资源。
+* 修改文件夹权限
+    * `sudo chmod -R 777 react-node-ant-design`
+* 安装 `egg-mysql` 使用数据库
+    * `npm i egg-mysql --save`
+* 安装下载mysql到mac上面
+    * https://dev.mysql.com/downloads/mysql/
+        * 注意下对版本，支持的你的系统版本
+    * 安装后配置环境变量
+        * `vim ~/.bash_profile`
+        * 输入 `export PATH=$PATH:/usr/local/mysql/bin export PATH=$PATH:/usr/local/mysql/support-files`
+        * 输入 `:wq` 退出
+        * 让配置生效 `source ~/.bash_profile `
+    * 启动mysql
+        * `sudo mysql.server stop restart status`
+        * 其实已经启动了
+        * 直接可以使用 `mysql -u root -p ` 进入mysql
+* 干掉某个端口的进程
+    * `lsof -i :7001` 7001 代表端口号，查询到对应的pid
+    * `kill -9 进程PID`
+
+```
+
+卸载mysql执行的命令
+
+sudo rm /usr/local/mysql
+
+sudo rm -rf /usr/local/mysql*
+
+sudo rm -rf /Library/StartupItems/MySQLCOM
+
+sudo rm -rf /Library/PreferencePanes/My*
+
+vim /etc/hostconfig  (and removed the line MYSQLCOM=-YES-)
+
+rm -rf ~/Library/PreferencePanes/My*
+
+sudo rm -rf /Library/Receipts/mysql*
+
+sudo rm -rf /Library/Receipts/MySQL*
+
+sudo rm -rf /var/db/receipts/com.mysql.*
+
+```
+
+
+* egg.js 目录结构介绍
+    * app文件夹:项目开发文件，程序员主要操作的文件，项目的大部分代码都会写在这里。
+    * config文件夹：这个是整个项目的配置目录，项目和服务端的配置都在这里边进行设置。
+    * logs文件夹：日志文件夹，正常情况下不用修改和查看里边内容。
+    * node_modules:项目所需要的模块文件，这个前端应该都非常了解，不多作介绍。
+    * run文件夹：运行项目时，生成的配置文件，基本不修改里边的文件。
+    * test文件夹：测试使用的配合文件，这个在测试时会使用。
+    * .autod.conf.js: egg.js自己生成的配置文件，不需要进行修改。
+    * eslinttrc和eslintignore：代码格式化的配置文件。
+    * gitgnore：git设置忽略管理的配置文件。
+    * package.json： 包管理和命令配置文件，这个文件经常进行配置。
+
+* egg.js 目录约定规范
+    * controller文件夹：控制器，渲染和简单的业务逻辑都会写道这个文件里。配置路由时也会用到（路由配置需要的文件都要写在控制器里）。
+    * public文件夹：公用文件夹，把一些公用资源都放在这个文件夹下。
+    * router.js: 项目的路由配置文件，当用户访问服务的时候，在没有中间件的情况下，会先访问router.js文件。
+    * service文件夹：这个是当我们的业务逻辑比较复杂或和数据库打交道时，会把业务逻辑放到这个文件中。
+    * view文件夹：模板文件夹，相当于表现层的专属文件夹，这个项目，我们使用接口的形式，所以不需要建立view文件夹。
+    * extend文件：当我们需要写一些模板中使用的扩展方法时，我们会放到这个文件夹里。
+    * middleware：中间件文件夹，用来写中间件的，比如最常用的路由首位。
---
+++ blog/components/markdown/MarkdownNavBar.js
@@ -0,0 +1,258 @@
+import 'core-js/shim';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class MarkdownNavbar extends Component {
+  static propTypes = {
+    source: PropTypes.string,
+    ordered: PropTypes.bool,
+    headingTopOffset: PropTypes.number,
+    updateHashAuto: PropTypes.bool,
+    declarative: PropTypes.bool,
+    className: PropTypes.string,
+  };
+
+  static defaultProps = {
+    source: '',
+    ordered: true,
+    headingTopOffset: 0,
+    updateHashAuto: true,
+    declarative: false,
+    className: '',
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentListNo: '',
+    };
+  }
+
+  componentDidMount() {
+    this._initheadingsId()
+    document.addEventListener('scroll', this._winScroll, false)
+  }
+
+//   UNSAFE_componentWillReceiveProps() {
+//     if (this.addTargetTimeout) {
+//       clearTimeout(this.addTargetTimeout);
+//     }
+//     this.addTargetTimeout = setTimeout(() => {
+//       this._initheadingsId();
+//       document.addEventListener('scroll', this._winScroll, false);
+//     }, 1e3);
+//   }
+
+  componentWillUnmount() {
+    if (this.addTargetTimeout) {
+      clearTimeout(this.addTargetTimeout);
+    }
+    if (this.scrollTimeout) {
+      clearTimeout(this.scrollTimeout);
+    }
+    document.removeEventListener('scroll', this._winScroll, false);
+  }
+
+  _getNavStructure() {
+    const contentWithoutCode = this.props.source.replace(
+      /```[^`\n]*\n+[^```]+```\n+/g,
+      ''
+    );
+
+    const pattOfTitle = /#+\s([^#\n]+)\n*/g;
+    const matchResult = contentWithoutCode.match(pattOfTitle);
+
+    if (!matchResult) {
+      return [];
+    }
+
+    const navData = matchResult.map((r, i) => ({
+      index: i,
+      level: r.match(/^#+/g)[0].length,
+      text: r.replace(pattOfTitle, '$1'),
+    }));
+
+    const levelCount = level => navData.filter(r => r.level === level).length;
+    let startLevel = 1;
+    let startLevelCount = levelCount(startLevel);
+    while (!startLevelCount) {
+      startLevel += 1;
+      startLevelCount = levelCount(startLevel);
+    }
+
+    let listNo = 1;
+    navData.forEach(t => {
+      if (t.level === startLevel) {
+        t.listNo = listNo.toString();
+        listNo += 1;
+      }
+    });
+
+    navData.forEach((t, i) => {
+      if (!t.listNo) {
+        if (navData[i - 1] && t.level === navData[i - 1].level + 1) {
+          t.listNo = `${navData[i - 1].listNo}.1`;
+        } else if (navData[i - 1] && t.level === navData[i - 1].level) {
+          t.listNo = navData[i - 1].listNo.replace(
+            /^(.+\.)(\d+)$/g,
+            (w, $1, $2) => `${$1}${parseInt($2, 10) + 1}`
+          );
+        } else {
+          t.listNo = '';
+        }
+      }
+    });
+
+    const result = navData.map(nav => ({
+      ...nav,
+      listNo: nav.listNo.indexOf('.') < 0 ? `${nav.listNo}.` : nav.listNo,
+      text: nav.text.replace(/^(\d+\.)+\s?/g, '').replace(/^\d+\.?\s?/g, ''),
+    }));
+
+    return result;
+  }
+
+  _scrollToTarget(dataId) {
+    if (this.scrollTimeout) {
+      clearTimeout(this.scrollTimeout);
+    }
+
+    // TODO: create one new `scrollTo` method for different browsers.
+    this.scrollTimeout = setTimeout(() => {
+      const target = document.querySelector(`[data-id="${dataId}"]`);
+      window.scrollTo(0, target.offsetTop - this.props.headingTopOffset);
+    }, 1e2);
+  }
+
+  _initheadingsId() {
+      debugger
+    this._getNavStructure().forEach(t => {
+      const headings = document.querySelectorAll(`h${t.level}`);
+      debugger
+      const curheading = Array.prototype.slice
+        .apply(headings)
+        .find(
+          h =>
+            h.innerText
+              .replace(/^(\d+\.)+\s?/g, '')
+              .replace(/^\d+\.?\s?/g, '') === t.text
+        );
+
+      if (curheading) {
+        curheading.dataset.id = this.props.declarative
+          ? t.text
+          : `heading-${t.index}`;
+      }
+
+      const headingId = this.props.declarative
+        ? window.location.hash.replace(/^#/, '').trim()
+        : (window.location.hash.match(/heading-\d+/g) || [])[0];
+
+      if (headingId && headingId === curheading.dataset.id) {
+        this._scrollToTarget(headingId);
+        this.setState({
+          currentListNo: t.listNo,
+        });
+      }
+    });
+  }
+
+  _getHeadingList() {
+    const headingList = [];
+
+    this._getNavStructure().forEach(t => {
+      const headings = document.querySelectorAll(`h${t.level}`);
+      const curheading = Array.prototype.slice
+        .apply(headings)
+        .find(
+          h =>
+            h.innerText
+              .replace(/^(\d+\.)+\s?/g, '')
+              .replace(/^\d+\.?\s?/g, '') === t.text
+        );
+      if (curheading) {
+        headingList.push({
+          dataId: this.props.declarative ? t.text : `heading-${t.index}`,
+          listNo: t.listNo,
+          offsetTop: curheading.offsetTop,
+        });
+      }
+    });
+
+    return headingList;
+  }
+
+  _winScroll = () => {
+    const scrollTop =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+
+    const newHeadingList = this._getHeadingList().map(h => ({
+      ...h,
+      distanceToTop: Math.abs(
+        scrollTop + this.props.headingTopOffset - h.offsetTop
+      ),
+    }));
+    const distanceList = newHeadingList.map(h => h.distanceToTop);
+    const minDistance = Math.min(...distanceList);
+    const curHeading = newHeadingList.find(
+      h => h.distanceToTop === minDistance
+    );
+
+    if (this.props.updateHashAuto) {
+      this._updateHash(curHeading.dataId);
+    }
+    this.setState({
+      currentListNo: curHeading.listNo,
+    });
+  };
+
+  _updateHash(value) {
+    history.replaceState(
+      {},
+      '',
+      `${window.location.pathname}${window.location.search}#${value}`
+    );
+  }
+
+  render() {
+    const tBlocks = this._getNavStructure().map(t => {
+      const cls = `title-anchor title-level${t.level} ${
+        this.state.currentListNo === t.listNo ? 'active' : ''
+      }`;
+
+      return (
+        <div
+          className={cls}
+          onClick={evt => {
+            evt.preventDefault();
+            this._updateHash(
+              this.props.declarative ? t.text : `heading-${t.index}`
+            );
+            this._scrollToTarget(
+              this.props.declarative ? t.text : `heading-${t.index}`
+            );
+            this.setState({
+              currentListNo: t.listNo,
+            });
+          }}
+          key={`title_anchor_${Math.random()
+            .toString(36)
+            .substring(2)}`}>
+          {this.props.ordered ? <small>{t.listNo}</small> : null}
+          {t.text}
+        </div>
+      );
+    });
+
+    return (
+      <div className={`markdown-navigation ${this.props.className}`}>
+        {tBlocks}
+      </div>
+    );
+  }
+}
+
+export default MarkdownNavbar;
