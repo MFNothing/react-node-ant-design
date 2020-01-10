9b1d7959d4687ab536c536e97122bb52eba6039b
lym <liuyimin@sobey.com>
2020-01-10 14:12:59 +0800

    admin 打包后静态文件

---
+++ build/adminBuild/precache-manifest.746b3e867746821cc3ede04911d27d68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d77f4eaf1a23c7c2dccfba705792fc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "55a8401572024760b88e",
+    "url": "./static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "b9533ff9fa4e2719e3fb",
+    "url": "./static/css/main.68b214fb.chunk.css"
+  },
+  {
+    "revision": "55a8401572024760b88e",
+    "url": "./static/js/2.4cbbbac5.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "./static/js/2.4cbbbac5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b9533ff9fa4e2719e3fb",
+    "url": "./static/js/main.ad8e8fe0.chunk.js"
+  },
+  {
+    "revision": "f1586010d79e4fd04c88",
+    "url": "./static/js/runtime-main.5fe64e04.js"
+  }
+]);
